d6b37a3da7cc531e6ecf47b5f4bbb63b2a3c39e7
RossLitz <codingclasses.049@gmail.com>
2015-03-16 14:11:14 -0500

    add genre to the book

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
 	 this.resource('book', { path: '/books/:book_id'});
+   
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -35,13 +36,19 @@ App.Book = DS.Model.extend({
 	 review: DS.attr(),
 	 rating: DS.attr('number'),
 	 amazon_id: DS.attr(),
-	 url: function(){
-	 	return "http://www.amazon.com/gp/product/"+this.get('amazon_id')
-	 }.property('amazon_id'),
-	 image:  function(){
-	 	return "http://images.amazon.com/images/P/"+this.get('amazon_id')+".01.ZTZZZZZZ.jpg"
-	 }.property('amazon_id')
+   genre: DS.belongsTo('genre'),
+   url: function(){
+    return "http://www.amazon.com/gp/product/"+this.get('amazon_id')
+   }.property('amazon_id'),
+   image:  function(){
+    return "http://images.amazon.com/images/P/"+this.get('amazon_id')+".01.ZTZZZZZZ.jpg"
+   }.property('amazon_id')
 }); 
+
+App.Genre = DS.Model.extend({
+  name: DS.attr(),
+  books: DS.hasMany('book')
+});
 
 App.Book.FIXTURES = [
   {
@@ -51,7 +58,7 @@ App.Book.FIXTURES = [
     review: 'Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.',
     rating: 5,
     amazon_id: '0465046746',
-    genre: 3
+    genre: 203
     
   },
   {
@@ -61,7 +68,7 @@ App.Book.FIXTURES = [
     review: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
     rating: 5,
     amazon_id: '0553283685',
-    genre: 1
+    genre: 202
   },
   {
     id: 3,
@@ -70,6 +77,23 @@ App.Book.FIXTURES = [
     review: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
     rating: 2,
     amazon_id: '159184617X',
-    genre: 3
+    genre: 201
+  }
+];
+
+App.Genre.FIXTURES = [
+  {
+    id: 201,
+    name: 'Scince-Fiction',   
+    books: [2]
+  },
+  {
+    id: 202,
+    name: 'Fiction',    
+  },
+  {
+    id: 203,
+    name: 'Non-Fiction',    
+    books: [1,3]
   }
 ];
